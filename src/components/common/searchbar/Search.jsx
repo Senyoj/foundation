0a1fefc73594b3f438962c691d492647c10912aa
Joel Senyo <116897993+Senyoj@users.noreply.github.com>
2023-10-31 22:34:59 -1000

    components update

---
+++ src/components/common/searchbar/Search.jsx
@@ -0,0 +1,13 @@
+import "./Search.css"
+
+import React from 'react'
+
+function Search() {
+  return (
+    <div>
+      search Bar
+    </div>
+  )
+}
+
+export default Search
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,11 @@
 import  './Home.css'
-
+import Header from "../../common/header/Header"
 import React from 'react'
 
 function Home() {
   return (
     <div className='body'>
-      hello world!!
+      <Header/>
     </div>
   )
 }
